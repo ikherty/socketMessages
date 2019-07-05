28245de45ff2731cc77c1b795cc42e24b9ede5fd
qw <ikherty@gmail.com>
2019-07-05 18:57:25 +0300

    done

--- venv/include/client.py
+++ venv/include/client.py
@@ -1,10 +1,8 @@
-# -*- coding: utf-8 -*-
-
 import socket
 
 sock = socket.socket()
 sock.connect(('localhost', 9090))
-sock.send('hello, world!'.encode())
+sock.send('ksoo'.encode())
 
 data = sock.recv(1024)
 sock.close()
